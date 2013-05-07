48899c09a8140f9b3d791ca735cfa3760b91db11
Adam Hutchison <adam@moneydesktop.com>
2013-05-06 22:12:15 -0600

    First cut at a cache support module.

--- lib/firebolt/cache.rb
+++ lib/firebolt/cache.rb
@@ -1,10 +1,40 @@
+require 'secure_random'
+
 require 'firebolt/cache/sweeper'
 require 'firebolt/cache/warmer'
 
 module Firebolt
   module Cache
 
-    KEYS_KEY = 'firebolt.keys'.freeze
+    def self.cache_key(type)
+      "#{::Firebolt.config.namespace}.#{type}"
+    end
 
+    def self.entry_key_prefix(salt = nil)
+      salt ||= self.salt
+      "#{cache_key(:entry)}.#{salt}"
+    end
+
+    def self.keys_key
+      cache_key(:keys)
+    end
+
+    def self.generate_salt
+      ::SecureRandom.hex
+    end
+
+    def self.reset_salt!(new_salt)
+      ::Firebolt.config.cache.write(self.salt_key, new_salt)
+    end
+
+    def self.salt
+      ::Firebolt.config.cache.fetch(self.salt_key) do
+        self.generate_salt
+      end
+    end
+
+    def self.salt_key
+      cache_key(:salt)
+    end
   end
 end
