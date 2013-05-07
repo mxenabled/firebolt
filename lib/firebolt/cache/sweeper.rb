a7dfa632c76ca6e6d1526715a6f201c5c2372553
Adam Hutchison <adam@moneydesktop.com>
2013-05-06 22:12:32 -0600

    First cut at a class to sweep the cache.

--- lib/firebolt/cache/sweeper.rb
+++ lib/firebolt/cache/sweeper.rb
@@ -1,19 +1,34 @@
 module Firebolt
   module Cache
     class Sweeper
+      attr_reader :entry_key_prefix_to_sweep
+
+      def initialize(entry_key_prefix_to_sweep)
+        @entry_key_prefix_to_sweep = entry_key_prefix_to_sweep
+      end
+
+      def sweep!
+        keys_to_sweep.each do |key|
+          cache.delete(key)
+        end
+      end
+
+    private
 
       def cache
         ::Firebolt.config.cache
       end
 
-      def known_keys
-        cache.get(::Firebolt::Cache::KEYS_KEY)
+      def key_matcher
+        /^#{::Regexp.escape(entry_key_prefix_to_sweep)}\./
       end
 
-      def sweep!
-        known_keys.each do |key|
-          cache.delete(key)
-        end
+      def keys_to_sweep
+        known_keys.grep(key_matcher)
+      end
+
+      def known_keys
+        cache.get(::Firebolt::Cache.keys_key) || []
       end
 
     end
