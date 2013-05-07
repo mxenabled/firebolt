b55a0e7cd8d6e413aa90d6454975881c9f10ce11
Adam Hutchison <adam@moneydesktop.com>
2013-05-06 22:12:46 -0600

    First cut at a class to warmer the cache.

--- lib/firebolt/cache/warmer.rb
+++ lib/firebolt/cache/warmer.rb
@@ -2,14 +2,47 @@ module Firebolt
   module Cache
     class Warmer
 
+      attr_reader :salt
+
+      def initialize(salt)
+        @salt = salt
+      end
+
       def cache
         ::Firebolt.config.cache
       end
 
       def warm
+        new_keys = []
+        results = warmer.call
 
+        raise RuntimeError, "Warmer must return an object that responds to #each_pair." unless results.respond_to?(:each_pair)
+
+        results.each_pair do |key, value|
+          cache_key = ::Firebolt::Cache.salted_cache_key(key)
+          cache.write(cache_key, value)
+          new_keys << cache_key
+        end
+
+        track_keys(new_keys)
       end
 
+      def warmer
+        ::Firebolt.config.warmer
+      end
+
+    private
+
+      def salted_cache_key(suffix)
+        "#{self.entry_salt_prefix_key}.#{suffix}"
+      end
+
+      def track_keys(new_keys)
+        keys = cache.read(::Firebolt::Cache.keys_key) || []
+        keys += new_keys
+
+        cache.write(::Firebolt::Cache.keys_key, keys)
+      end
     end
   end
 end
