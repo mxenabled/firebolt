7c443d57b231f0080a354b7814e94ed081e81373
Adam Hutchison <adam@moneydesktop.com>
2013-05-07 09:27:28 -0600

    Cache sweeper now creates the matcher on initialize.

--- lib/firebolt/cache/sweeper.rb
+++ lib/firebolt/cache/sweeper.rb
@@ -1,10 +1,10 @@
 module Firebolt
   module Cache
     class Sweeper
-      attr_reader :entry_key_prefix_to_sweep
+      attr_reader :matcher
 
-      def initialize(entry_key_prefix_to_sweep)
-        @entry_key_prefix_to_sweep = entry_key_prefix_to_sweep
+      def initialize(matcher)
+        @matcher = /^#{::Regexp.escape(matcher)}\./
       end
 
       def sweep!
@@ -19,18 +19,13 @@ module Firebolt
         ::Firebolt.config.cache
       end
 
-      def key_matcher
-        /^#{::Regexp.escape(entry_key_prefix_to_sweep)}\./
-      end
-
       def keys_to_sweep
-        known_keys.grep(key_matcher)
+        known_keys.grep(matcher)
       end
 
       def known_keys
         cache.get(::Firebolt::Cache.keys_key) || []
       end
-
     end
   end
 end
