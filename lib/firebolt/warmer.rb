1f350617548491b9635ec6bc4a1952338c83ad22
Adam Hutchison <adam@moneydesktop.com>
2013-05-07 21:48:36 -0600

    Rework CacheWarmer into an inheritable intereface.
    Having a cache warmer that works from a file warmer doesn't make sense.
    If the file warmer were a warmer, it would have the ability to write
    it's contents to the cache.
    
    This reworks CacheWarmer to be simply warmer, which
    expects to be inherited by a class that defines `#perform` (similar to
    SuckerPunch), instead of `#call` like before.

---
+++ lib/firebolt/warmer.rb
@@ -0,0 +1,57 @@
+module Firebolt
+  class Warmer
+    ##
+    # Public class methods
+    #
+    def self.warm
+      warmer = self.new
+      warmer.warm
+    end
+
+    ##
+    # Public instance methods
+    #
+    def warm
+      results = perform
+
+      write_results_to_cache(results)
+      reset_salt!
+
+      results
+    end
+
+  private
+
+    ##
+    # Private instance methods
+    #
+    def expires_in
+      @expires_in ||= ::Firebolt.config.frequency + 1.hour
+    end
+
+    def raise_failed_result
+      raise "Warmer must return an object that responds to #each_pair."
+    end
+
+    def reset_salt!
+      ::Firebolt::Cache.reset_salt!(salt)
+    end
+
+    def salt
+      @salt ||= ::SecureRandom.hex
+    end
+
+    def salted_cache_key(suffix)
+      ::Firebolt::Cache.cache_key_with_salt(suffix, salt)
+    end
+
+    def write_results_to_cache(results)
+      raise_failed_result unless results.respond_to?(:each_pair)
+
+      results.each_pair do |key, value|
+        cache_key = salted_cache_key(key)
+        ::Firebolt.config.cache.write(cache_key, value, :expires_in => expires_in)
+      end
+    end
+  end
+end
